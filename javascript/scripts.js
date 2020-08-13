f93888fc49109264de9b220ce8afa8468ec19784
Igor Santos <igor.santos@actualsales.com.br>
2020-08-13 20:52:17 -0300

    Starting creating the 'Add item to cart' functionality

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,12 +1,83 @@
-var cartIcon = document.querySelector("#cart-button");
+(function () {
 
-cartIcon.addEventListener('click', function() {
-    document.querySelector('#cart-sidebar').style.display = 'block'; 
-})
+    function showHideCart() {
+        const showCartBtn = document.querySelector("#show-cart-button");
+        const hideCartBtn = document.querySelector(".hide-cart-button");
+        
+        showCartBtn.addEventListener('click', function() {
+            document.querySelector('#cart-sidebar').style.display = 'block'; 
+        });
+        
+        hideCartBtn.addEventListener('click', function() {
+            document.querySelector('#cart-sidebar').style.display = 'none'; 
+        });
+    }
+    showHideCart()
 
-var cartClose = document.querySelector(".cart-sidebar-close");
+    const addToCartBtn = document.querySelectorAll('.js-add-cart-btn');
 
-cartClose.addEventListener('click', function() {
-    document.querySelector('#cart-sidebar').style.display = 'none'; 
-})
+    for (let i = 0; i < addToCartBtn.length; i++) {
+        var itemImage, itemName, itemStars, itemPrice, itemQuantity;
 
+        function getItemInfo() {
+            addToCartBtn[i].addEventListener('click', function() {
+                itemImage = addToCartBtn[i].parentNode.parentNode.querySelector(".js-item-image").getAttribute("src");
+                itemName = addToCartBtn[i].parentNode.querySelector(".js-item-name").innerText;
+                itemStars = addToCartBtn[i].parentNode.querySelector(".js-item-stars").getAttribute("src");
+                itemPrice = addToCartBtn[i].parentNode.querySelector(".js-item-price").innerText;
+                itemQuantity = addToCartBtn[i].parentNode.querySelector(".js-item-quantity").value;
+                
+                addItemToCart();
+            });
+        }
+        getItemInfo();
+
+        function addItemToCart() {
+            let cartItemTemplate = 
+            `
+            <div class="cart-item">
+                <figure class="cart-item-img">
+                    <img src="${itemImage}" alt="Imagem da máquina de lavar">
+                </figure>
+                <div class="cart-item-info">
+                    <a href="#" target="_blank">
+                        ${itemName}
+                    </a>
+                    <figure>
+                        <img src="${itemStars}" alt="Esse produto possui avaliação de 5 estrelas">
+                    </figure>
+                    <h3>${itemPrice}</h3>
+                    <input type="number" name="item_quantity" id="item_quantity" min="1" max="99" value="${itemQuantity}">
+                    <a href="#" class="btn-primary">Excluir</a>
+                </div>                   
+            </div>
+            `
+            
+        const cartItems = document.querySelector(".cart-items-list");
+    
+        cartItems.insertAdjacentHTML('afterbegin' , cartItemTemplate);
+        }
+    } 
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
