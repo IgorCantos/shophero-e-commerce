4e3fe72b886145385251f3707e417b628113ab69
Igor Santos <igor.santos@actualsales.com.br>
2020-09-28 20:16:20 -0300

    Updating updateCartTotal() to transform a number in brazilian money

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -57,7 +57,7 @@
             `
             insertCartItem = document.querySelector(".cart-items-list");
 
-            // Check if item already exists in the cart
+            // Checa se o item já existe no carrinho
             if (insertCartItem.innerHTML.indexOf(itemName) != -1) {
                 alert('Este item já existe no seu carrinho.')
                 return;
@@ -88,31 +88,42 @@
                let cartItem, cartItemPrice;
 
                 cartItem = allCartItems[i];
-
-                cartItemPrice = cartItem.querySelector(".js-item-price").innerText;
-                cartItemPrice = getMoney(cartItemPrice);
-                formatMoney = formatReal(cartItemPrice)
-                toNumber = formatMoney;
+                cartItemPrice = parseFloat(cartItem.querySelector(".js-item-price").innerText.replace("R$", "").replace(",", "").replace(".", ""));
                 cartItemQuantity = cartItem.querySelector(".js-item-quantity").value;
-                
-                cartTotal = cartTotal + (toNumber * cartItemQuantity);
+                cartTotal += (cartItemPrice * cartItemQuantity);
+
+                total = formatReal(cartTotal)
             }
 
-            document.querySelector('#cart-total').innerHTML = '<strong>Total</strong>: ' + 'R$ ' + cartTotal;
+            document.querySelector('#cart-total').innerHTML = '<strong>Total</strong>: ' + total;
         };
 
-        function getMoney(str) {
-            return parseInt(str.replace(/[\D]+/g,''));
-        }
 
-        function formatReal(int) {
+        // Transforma númeoro em moeda brasileira
+        function formatReal( int ) {
             var tmp = int + '';
-            tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
-
-            if (tmp.length > 6) {
-                tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+            var neg = false;
+            if (tmp.indexOf("-") == 0) {
+                neg = true;
+                tmp = tmp.replace("-","");
             }
-            return tmp;
+
+            if(tmp.length == 1) tmp = "0" + tmp
+
+            tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
+            if( tmp.length > 6)
+                tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+
+            if( tmp.length > 9)
+                tmp = tmp.replace(/([0-9]{3}).([0-9]{3}),([0-9]{2}$)/g,".$1.$2,$3");
+
+            if( tmp.length > 12)
+                tmp = tmp.replace(/([0-9]{3}).([0-9]{3}).([0-9]{3}),([0-9]{2}$)/g,".$1.$2.$3,$4");
+
+            if(tmp.indexOf(".") == 0) tmp = tmp.replace(".","");
+            if(tmp.indexOf(",") == 0) tmp = tmp.replace(",","0,");
+
+            return (neg ? '-' + tmp : tmp);
         }
 
         showHideCart();
